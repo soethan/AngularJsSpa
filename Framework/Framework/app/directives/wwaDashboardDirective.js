54e474a16fa66f503fc48cd66249e9cdadbb59cf
Soe Than <soethan@gmail.com>
2015-09-19 23:15:32 +0800

    Dashboard Creation

--- Framework/Framework/app/directives/wwaDashboardDirective.js
+++ Framework/Framework/app/directives/wwaDashboardDirective.js
@@ -4,6 +4,22 @@ angular.module("app").directive("wwaDashboard", [function () {
         scope: {
 
         },
-        template: "<h1>Dashboard Page</h1>"
+        template: "<ps-dashboard></ps-dashboard>",
+        link: function (scope) {
+            scope.widgets = [
+                {
+                    sizeX: 3,
+                    sizeY: 3,
+                    row: 0,
+                    col: 0
+                },
+                {
+                    sizeX: 2,
+                    sizeY: 4,
+                    row: 0,
+                    col: 5
+                }
+            ];
+        }
     };
 }]);
---
+++ Framework/Framework/ext-modules/psDashboard/psDashboardDirective.js
@@ -0,0 +1,6 @@
+﻿"use strict";
+angular.module("psDashboard").directive("psDashboard", function () {
+    return {
+        templateUrl: "ext-modules/psDashboard/psDashboardTemplate.html"
+    };
+});
