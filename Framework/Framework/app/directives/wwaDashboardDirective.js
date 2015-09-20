eb6c5f4d18f7f05e8a30e7f8f86944aae3166f7b
Soe Than <soethan@gmail.com>
2015-09-20 22:27:32 +0800

    psWidgetBody

--- Framework/Framework/app/directives/wwaDashboardDirective.js
+++ Framework/Framework/app/directives/wwaDashboardDirective.js
@@ -23,15 +23,16 @@ angular.module("app").directive("wwaDashboard", [function () {
                     sizeX: 3,
                     sizeY: 3,
                     row: 0,
-                    col: 0
-                },
-                {
-                    title: "Second",
-                    sizeX: 2,
-                    sizeY: 4,
-                    row: 0,
-                    col: 5
-                }
+                    col: 0,
+                    template: "<wwa-temperature></wwa-temperature>"
+                }//,
+                //{
+                //    title: "Second",
+                //    sizeX: 2,
+                //    sizeY: 4,
+                //    row: 0,
+                //    col: 5
+                //}
             ];
         }
     };
