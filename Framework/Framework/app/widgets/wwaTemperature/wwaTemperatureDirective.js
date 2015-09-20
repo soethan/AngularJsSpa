390567acfbc9140f47c83e21a90f83c5572961b1
Soe Than <soethan@gmail.com>
2015-09-20 17:49:49 +0800

    wwaTemperature widget

---
+++ Framework/Framework/app/widgets/wwaTemperature/wwaTemperatureDirective.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+angular.module("app").directive("wwaTemperature", ["dataService", function (dataService) {
+    return {
+        templateUrl: "app/widgets/wwaTemperature/wwaTemperatureTemplate.html",
+        link: function (scope, el, attrs) {
+
+        }
+    };
+}]);
