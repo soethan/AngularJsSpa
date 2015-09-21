e4f671bc729c9f6761818f233d5a433be72afd09
soethan <soethan@gmail.com>
2015-09-21 17:37:44 +0800

    Fix background image loading

--- Framework/Framework/app/widgets/wwaTemperature/wwaTemperatureDirective.js
+++ Framework/Framework/app/widgets/wwaTemperature/wwaTemperatureDirective.js
@@ -3,6 +3,8 @@ angular.module("app").directive("wwaTemperature", ["dataService", function (data
     return {
         templateUrl: "app/widgets/wwaTemperature/wwaTemperatureTemplate.html",
         link: function (scope, el, attrs) {
+            scope.selectedLocation = null;
+
             dataService.getLocation(scope.item.widgetSettings.id).then(function (data) {
                 scope.selectedLocation = data;
             });
